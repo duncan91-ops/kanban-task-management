6ffaeae449215de835a3b7af01a04eda8a142e67
Duncan Kithinji <duncankithinji91@gmail.com>
2023-03-11 11:56:55 +0300

    split settings into base, development & productions settings and configure env variables

---
+++ server/kanban_project/settings/development.py
@@ -0,0 +1,16 @@
+from .base import *
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": env("POSTGRES_ENGINE"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
+    }
+}
